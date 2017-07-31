94f96795093951327e6c8fc1d9659a0c14a4ffb5
Nicolas Raynaud <github@nraynaud.com>
2017-07-31 20:16:15 +0200

    XOSAN: select brick size at SR creation

--- src/common/xo/index.js
+++ src/common/xo/index.js
@@ -1956,13 +1956,14 @@ export const setIpPool = (ipPool, { name, addresses, networks }) => (
 
 export const getVolumeInfo = (xosanSr, infoType) => _call('xosan.getVolumeInfo', { sr: xosanSr, infoType })
 
-export const createXosanSR = ({ template, pif, vlan, srs, glusterType, redundancy }) => _call('xosan.createSR', {
+export const createXosanSR = ({ template, pif, vlan, srs, glusterType, redundancy, brickSize }) => _call('xosan.createSR', {
   template,
   pif: pif.id,
   vlan: String(vlan),
   srs: resolveIds(srs),
   glusterType,
-  redundancy: Number.parseInt(redundancy)
+  redundancy: Number.parseInt(redundancy),
+  brickSize
 })
 
 export const addXosanBricks = (xosansr, lvmsrs) => _call('xosan.addBricks', {xosansr, lvmsrs})
@@ -1970,7 +1971,7 @@ export const addXosanBricks = (xosansr, lvmsrs) => _call('xosan.addBricks', {xos
 export const replaceXosanBrick = (xosansr, previousBrick, newLvmSr) => _call('xosan.replaceBrick', {xosansr, previousBrick, newLvmSr})
 export const removeXosanBricks = (xosansr, bricks) => _call('xosan.removeBricks', {xosansr, bricks})
 
-export const computeXosanPossibleOptions = lvmSrs => _call('xosan.computeXosanPossibleOptions', { lvmSrs })
+export const computeXosanPossibleOptions = (lvmSrs, brickSize) => _call('xosan.computeXosanPossibleOptions', { lvmSrs, brickSize })
 
 import InstallXosanPackModal from './install-xosan-pack-modal' // eslint-disable-line import/first
 export const downloadAndInstallXosanPack = pool =>
