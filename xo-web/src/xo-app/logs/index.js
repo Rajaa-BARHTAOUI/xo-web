5ad933efe421d1f8b62c271e5f7e466ed6fbccbb
Rajaa Barhtaoui <rajaa.barhtaoui@gmail.com>
2017-10-24 09:32:05 +0200

    newVersion xo-web

---
+++ xo-web/src/xo-app/logs/index.js
@@ -0,0 +1,339 @@
+import _, { FormattedDuration } from 'intl'
+import ActionButton from 'action-button'
+import ActionRowButton from 'action-row-button'
+import BaseComponent from 'base-component'
+import ButtonGroup from 'button-group'
+import classnames from 'classnames'
+import Icon from 'icon'
+import NoObjects from 'no-objects'
+import propTypes from 'prop-types-decorator'
+import React, { Component } from 'react'
+import renderXoItem from 'render-xo-item'
+import SortedTable from 'sorted-table'
+import Tooltip from 'tooltip'
+import { alert, confirm } from 'modal'
+import { createGetObject } from 'selectors'
+import { FormattedDate } from 'react-intl'
+import {
+  connectStore,
+  formatSize,
+  formatSpeed
+} from 'utils'
+import {
+  Card,
+  CardHeader,
+  CardBlock
+} from 'card'
+import {
+  forEach,
+  get,
+  includes,
+  isEmpty,
+  map,
+  orderBy
+  } from 'lodash'
+import {
+  deleteJobsLog,
+  subscribeJobsLogs
+} from 'xo'
+
+// ===================================================================
+
+const jobKeyToLabel = {
+  continuousReplication: _('continuousReplication'),
+  deltaBackup: _('deltaBackup'),
+  disasterRecovery: _('disasterRecovery'),
+  genericTask: _('customJob'),
+  rollingBackup: _('backup'),
+  rollingSnapshot: _('rollingSnapshot')
+}
+
+// ===================================================================
+
+@connectStore(() => ({object: createGetObject()}))
+class JobParam extends Component {
+  render () {
+    const {
+      object,
+      paramKey,
+      id
+    } = this.props
+
+    return object != null
+      ? _.keyValue(object.type || paramKey, renderXoItem(object))
+      : _.keyValue(paramKey, String(id))
+  }
+}
+
+@connectStore(() => ({object: createGetObject()}))
+class JobReturn extends Component {
+  render () {
+    const {
+      object,
+      id
+    } = this.props
+
+    return <span><Icon icon='arrow-right' />{' '}{object ? renderXoItem(object) : String(id)}</span>
+  }
+}
+
+const JobCallStateInfos = ({ end, error }) => {
+  const [ icon, tooltip ] = error !== undefined
+    ? ['halted', 'failedJobCall']
+    : end !== undefined
+      ? ['running', 'successfulJobCall']
+      : ['busy', 'jobCallInProgess']
+
+  return <Tooltip content={_(tooltip)}>
+    <Icon icon={icon} />
+  </Tooltip>
+}
+
+const JobTransferredDataInfos = ({ start, end, size }) => <div>
+  <span><strong>{_('jobTransferredDataSize')}</strong> {formatSize(size)}</span>
+  <br />
+  <span><strong>{_('jobTransferredDataSpeed')}</strong> {formatSpeed(size, end - start)}</span>
+</div>
+
+const CALL_FILTER_OPTIONS = [
+  {label: 'successfulJobCall', value: 'success'},
+  {label: 'failedJobCall', value: 'error'},
+  {label: 'jobCallInProgess', value: 'running'},
+  {label: 'allJobCalls', value: 'all'}
+]
+
+const PREDICATES = {
+  all: () => true,
+  error: call => call.error !== undefined,
+  running: call => call.end === undefined && call.error === undefined,
+  success: call => call.end !== undefined && call.error === undefined
+}
+
+class Log extends BaseComponent {
+  state = {
+    filter: 'all'
+  }
+
+  render () {
+    const { props, state } = this
+    const predicate = PREDICATES[state.filter]
+
+    return <div>
+      <select
+        className='form-control'
+        onChange={this.linkState('filter')}
+        value={state.filter}
+      >
+        {map(CALL_FILTER_OPTIONS, ({ label, value }) => _(
+          { key: value },
+          label,
+          message => <option value={value}>{message}</option>
+        ))}
+      </select>
+      <br />
+      <ul className='list-group'>
+        {map(props.log.calls, call => {
+          const {
+            end,
+            error,
+            returnedValue,
+            start
+          } = call
+
+          let id
+          if (returnedValue != null) {
+            id = returnedValue.id
+            if (id === undefined && typeof returnedValue === 'string') {
+              id = returnedValue
+            }
+          }
+
+          return predicate(call) && <li key={call.callKey} className='list-group-item'>
+            <strong className='text-info'>{call.method}: </strong><JobCallStateInfos end={end} error={error} /><br />
+            {map(call.params, (value, key) => [ <JobParam id={value} paramKey={key} key={key} />, <br /> ])}
+            {end !== undefined && _.keyValue(_('jobDuration'), <FormattedDuration duration={end - start} />)}
+            {returnedValue != null && returnedValue.size !== undefined && <JobTransferredDataInfos start={start} end={end} size={returnedValue.size} />}
+            {id !== undefined && <span>{' '}<JobReturn id={id} /></span>}
+            {call.error &&
+              <span className='text-danger'>
+                <Icon icon='error' />
+                {' '}
+                {call.error.message
+                  ? <strong>{call.error.message}</strong>
+                  : JSON.stringify(call.error)
+                }
+              </span>}
+          </li>
+        })}
+      </ul>
+    </div>
+  }
+}
+
+const showCalls = log => alert(_('jobModalTitle', { job: log.jobId }), <Log log={log} />)
+
+const LOG_COLUMNS = [
+  {
+    name: _('jobId'),
+    itemRenderer: log => log.jobId,
+    sortCriteria: log => log.jobId
+  },
+  {
+    name: _('jobType'),
+    itemRenderer: log => jobKeyToLabel[log.key],
+    sortCriteria: log => log.key
+  },
+  {
+    name: _('jobTag'),
+    itemRenderer: log => get(log, 'calls[0].params.tag'),
+    sortCriteria: log => get(log, 'calls[0].params.tag')
+  },
+  {
+    name: _('jobStart'),
+    itemRenderer: log => log.start && <FormattedDate value={new Date(log.start)} month='short' day='numeric' year='numeric' hour='2-digit' minute='2-digit' second='2-digit' />,
+    sortCriteria: log => log.start,
+    sortOrder: 'desc'
+  },
+  {
+    default: true,
+    name: _('jobEnd'),
+    itemRenderer: log => log.end && <FormattedDate value={new Date(log.end)} month='short' day='numeric' year='numeric' hour='2-digit' minute='2-digit' second='2-digit' />,
+    sortCriteria: log => log.end || log.start,
+    sortOrder: 'desc'
+  },
+  {
+    name: _('jobDuration'),
+    itemRenderer: log => log.duration && <FormattedDuration duration={log.duration} />,
+    sortCriteria: log => log.duration
+  },
+  {
+    name: _('jobStatus'),
+    itemRenderer: log => <span>
+      {log.status === 'finished' &&
+        <span className={classnames('tag', {'tag-success': !log.hasErrors, 'tag-danger': log.hasErrors})}>{_('jobFinished')}</span>
+      }
+      {log.status === 'started' &&
+        <span className='tag tag-warning'>{_('jobStarted')}</span>
+      }
+      {(log.status !== 'started' && log.status !== 'finished') &&
+        <span className='tag tag-default'>{_('jobUnknown')}</span>
+      }
+      {' '}
+      <span className='pull-right'>
+        <ButtonGroup>
+          <Tooltip content={_('logDisplayDetails')}><ActionRowButton icon='preview' handler={showCalls} handlerParam={log} /></Tooltip>
+          <Tooltip content={_('remove')}><ActionRowButton handler={deleteJobsLog} handlerParam={log.logKey} icon='delete' /></Tooltip>
+        </ButtonGroup>
+      </span>
+    </span>,
+    sortCriteria: log => log.hasErrors ? ' ' : log.status
+  }
+]
+
+@propTypes({
+  jobKeys: propTypes.array.isRequired
+})
+export default class LogList extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      logsToClear: []
+    }
+    this.filters = {
+      onError: 'error',
+      successful: 'success'
+    }
+  }
+
+  componentWillMount () {
+    this.componentWillUnmount = subscribeJobsLogs(rawLogs => {
+      const logs = {}
+      const logsToClear = []
+      forEach(rawLogs, (log, logKey) => {
+        const data = log.data
+        const { time } = log
+        if (data.event === 'job.start' && includes(this.props.jobKeys, data.key)) {
+          logsToClear.push(logKey)
+          logs[logKey] = {
+            logKey,
+            jobId: data.jobId.slice(4, 8),
+            key: data.key,
+            userId: data.userId,
+            start: time,
+            calls: {},
+            time
+          }
+        } else {
+          const runJobId = data.runJobId
+          const entry = logs[runJobId]
+          if (!entry) {
+            return
+          }
+          logsToClear.push(logKey)
+          if (data.event === 'job.end') {
+            entry.end = time
+            entry.duration = time - entry.start
+            entry.status = 'finished'
+          } else if (data.event === 'jobCall.start') {
+            entry.calls[logKey] = {
+              callKey: logKey,
+              params: data.params,
+              method: data.method,
+              start: time,
+              time
+            }
+          } else if (data.event === 'jobCall.end') {
+            const call = entry.calls[data.runCallId]
+
+            if (data.error) {
+              call.error = data.error
+              entry.hasErrors = true
+              entry.meta = 'error'
+            } else {
+              call.returnedValue = data.returnedValue
+              call.end = time
+            }
+          }
+        }
+      })
+
+      forEach(logs, log => {
+        if (log.end === undefined) {
+          log.status = 'started'
+        } else if (!log.meta) {
+          log.meta = 'success'
+        }
+        log.calls = orderBy(log.calls, ['time'], ['desc'])
+      })
+
+      this.setState({
+        logs: orderBy(logs, ['time'], ['desc']),
+        logsToClear
+      })
+    })
+  }
+
+  _deleteAllLogs = () => {
+    return confirm({
+      title: _('removeAllLogsModalTitle'),
+      body: <p>{_('removeAllLogsModalWarning')}</p>
+    }).then(() => deleteJobsLog(this.state.logsToClear))
+  }
+
+  render () {
+    const { logs } = this.state
+
+    return (
+      <Card>
+        <CardHeader>
+          <Icon icon='log' /> Logs<span className='pull-right'><ActionButton disabled={isEmpty(logs)} btnStyle='danger' handler={this._deleteAllLogs} icon='delete' /></span>
+        </CardHeader>
+        <CardBlock>
+          <NoObjects collection={logs} emptyMessage={_('noLogs')}>
+            <SortedTable collection={logs} columns={LOG_COLUMNS} filters={this.filters} />
+          </NoObjects>
+        </CardBlock>
+      </Card>
+    )
+  }
+}
---
+++ xo-web/src/xo-app/settings/remotes/index.js
@@ -0,0 +1,402 @@
+import _, { messages } from 'intl'
+import ActionButton from 'action-button'
+import ActionRowButton from 'action-row-button'
+import filter from 'lodash/filter'
+import Icon from 'icon'
+import isEmpty from 'lodash/isEmpty'
+import map from 'lodash/map'
+import React, { Component } from 'react'
+import some from 'lodash/some'
+import StateButton from 'state-button'
+import Tooltip from 'tooltip'
+import { addSubscriptions } from 'utils'
+import { alert } from 'modal'
+import { error } from 'notification'
+import { format, parse } from 'xo-remote-parser'
+import { Password, Text } from 'editable'
+import { injectIntl } from 'react-intl'
+
+import {
+  createRemote,
+  deleteRemote,
+  disableRemote,
+  editRemote,
+  enableRemote,
+  subscribeRemotes,
+  testRemote
+} from 'xo'
+
+const remoteTypes = {
+  file: 'remoteTypeLocal',
+  nfs: 'remoteTypeNfs',
+  smb: 'remoteTypeSmb'
+}
+
+class AbstractRemote extends Component {
+  _changeUrlElement = (value, element) => {
+    const remote = {...this.props.remote}
+    remote[element] = value
+    const url = format(remote)
+    return editRemote(remote, {url})
+  }
+
+  _showError = () => alert(
+    _('remoteConnectionFailed'),
+    this.props.remote.error
+  )
+
+  _changeName = name => {
+    const { remote } = this.props
+    return editRemote(remote, {name})
+  }
+
+  _test = () => {
+    const { remote } = this.props
+    testRemote(remote).then(answer => {
+      const title = <span>
+        <Icon icon={answer.success ? 'success' : 'error'} />
+        {' '}
+        {_(answer.success ? 'remoteTestSuccess' : 'remoteTestFailure', {name: remote.name})}
+      </span>
+      let body
+      if (answer.success) {
+        body = _('remoteTestSuccessMessage')
+      } else {
+        body = <p>
+          <dl className='dl-horizontal'>
+            <dt>{_('remoteTestError')}</dt>
+            <dd>{answer.error}</dd>
+            <dt>{_('remoteTestStep')}</dt>
+            <dd>{answer.step}</dd>
+          </dl>
+        </p>
+      }
+      alert(title, body)
+    })
+  }
+
+  render () {
+    const {
+      remote
+    } = this.props
+
+    return <tr>
+      <td />
+      <td><Text value={remote.name} onChange={this._changeName} placeholder={this.props.intl.formatMessage(messages.remoteNamePlaceHolder)} /></td>
+      <td>{this._renderRemoteInfo(remote)}</td>
+      <td>{this._renderAuthInfo(remote)}</td>
+      <td>
+        <StateButton
+          disabledLabel={_('remoteDisconnected')}
+          disabledHandler={enableRemote}
+          disabledTooltip={_('remoteConnectTip')}
+
+          enabledLabel={_('remoteConnected')}
+          enabledHandler={disableRemote}
+          enabledTooltip={_('remoteDisconnectTip')}
+
+          handlerParam={remote}
+          state={remote.enabled}
+        />
+        {' '}
+        {remote.error &&
+          <Tooltip content={_('remoteConnectionFailed')}>
+            <a
+              className='text-danger btn btn-link'
+              style={{ padding: '0px' }}
+              onClick={this._showError}
+            >
+              <Icon
+                icon='alarm'
+                size='lg'
+              />
+            </a>
+          </Tooltip>
+        }
+      </td>
+      <td className='text-xs-right'>
+        {remote.enabled &&
+          <Tooltip content={_('remoteTestTip')}>
+            <ActionRowButton btnStyle='primary' handler={this._test} icon='diagnosis' />
+          </Tooltip>
+        }
+        {' '}
+        <Tooltip content={_('remoteDeleteTip')}>
+          <ActionRowButton btnStyle='danger' handler={deleteRemote} handlerParam={remote} icon='delete' />
+        </Tooltip>
+      </td>
+    </tr>
+  }
+
+  _renderRemoteInfo () {
+    throw new Error('NOT IMPLEMENTED')
+  }
+
+  _renderAuthInfo () {
+    throw new Error('NOT IMPLEMENTED')
+  }
+
+  get accessible () {
+    throw new Error('NOT IMPLEMENTED')
+  }
+
+  get unaccessible () {
+    throw new Error('NOT IMPLEMENTED')
+  }
+}
+
+@injectIntl
+class LocalRemote extends AbstractRemote {
+  _renderRemoteInfo () {
+    const { remote } = this.props
+    return <Text value={remote.path} onChange={v => this._changeUrlElement(v, 'path')} placeholder={this.props.intl.formatMessage(messages.remoteLocalPlaceHolderPath)} />
+  }
+
+  _renderAuthInfo () {
+    return ''
+  }
+
+  get accessible () {
+    return 'Accessible'
+  }
+
+  get unaccessible () {
+    return 'Unaccessible'
+  }
+}
+
+@injectIntl
+class NfsRemote extends AbstractRemote {
+  _renderRemoteInfo () {
+    const { remote } = this.props
+    return <span>
+      <Text value={remote.host} onChange={v => this._changeUrlElement(v, 'host')} placeholder={this.props.intl.formatMessage(messages.remoteNfsPlaceHolderHost)} />
+      :
+      <Text value={remote.path} onChange={v => this._changeUrlElement(v, 'path')} placeholder={this.props.intl.formatMessage(messages.remoteNfsPlaceHolderPath)} />
+    </span>
+  }
+
+  _renderAuthInfo () {
+    return ''
+  }
+
+  get accessible () {
+    return _('remoteMounted')
+  }
+
+  get unaccessible () {
+    return _('remoteUnmounted')
+  }
+}
+
+@injectIntl
+class SmbRemote extends AbstractRemote {
+  _renderRemoteInfo () {
+    const { remote } = this.props
+    return <span>
+      <strong className='text-info'>\\</strong>
+      <Text value={remote.host} onChange={v => this._changeUrlElement(v, 'host')} />
+      <strong className='text-info'>\</strong>
+      <span>
+        <Text value={remote.path} onChange={v => this._changeUrlElement(v, 'path')} placeholder={this.props.intl.formatMessage(messages.remoteSmbPlaceHolderRemotePath)} />
+      </span>
+    </span>
+  }
+
+  _renderAuthInfo () {
+    const { remote } = this.props
+    return <span>
+      <Text value={remote.username} onChange={v => this._changeUrlElement(v, 'username')} />
+      :
+      <Password value='' onChange={v => this._changeUrlElement(v, 'password')} placeholder={this.props.intl.formatMessage(messages.remotePlaceHolderPassword)} />
+      @
+      <Text value={remote.domain} onChange={v => this._changeUrlElement(v, 'domain')} />
+    </span>
+  }
+
+  get accessible () {
+    return 'Accessible'
+  }
+
+  get unaccessible () {
+    return 'Unaccessible'
+  }
+}
+
+@addSubscriptions({
+  remotes: cb => subscribeRemotes(rawRemotes => {
+    rawRemotes = map(rawRemotes, remote => ({...remote, ...parse(remote.url)}))
+    const remotes = {}
+    for (const remoteType in remoteTypes) {
+      remotes[remoteType] = filter(rawRemotes, r => r.type === remoteType)
+    }
+    cb(remotes)
+  })
+})
+
+@injectIntl
+export default class Remotes extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      type: 'file'
+    }
+  }
+
+  _handleRemoteTypeSelection = type => this.setState({type})
+
+  _checkNameExists = () => some(
+    this.props.remotes,
+    values => some(values, ['name', this.refs.name.value])
+  )
+    ? alert(
+      <span><Icon icon={'error'} />{' '}{_('remoteTestName')}</span>,
+      <p>{_('remoteTestNameFailure')}</p>
+    )
+    : this._createRemote()
+
+  _createRemote = async () => {
+    const {
+      name,
+      host,
+      path,
+      username,
+      password,
+      domain
+    } = this.refs
+    const {
+      type
+    } = this.state
+
+    const urlParams = {
+      type,
+      host: host && host.value,
+      path: path && path.value
+    }
+    username && (urlParams.username = username.value)
+    password && (urlParams.password = password.value)
+    domain && (urlParams.domain = domain.value)
+
+    const url = format(urlParams)
+    return createRemote(name && name.value, url).then(() => {
+      this.setState({type: 'file'})
+      path && (path.value = '')
+      username && (username.value = '')
+      password && (password.value = '')
+      domain && (domain.value = '')
+    }, err => error('Create Remote', err.message || String(err)))
+  }
+
+  render () {
+    const { remotes = {} } = this.props
+    const { type } = this.state
+
+    return (
+      <div>
+        <table className='table table-hover'>
+          {!isEmpty(remotes.file) &&
+            <tbody>
+              <tr>
+                <th className='text-info'>{_('remoteTypeLocal')}</th>
+                <th>{_('remoteName')}</th>
+                <th>{_('remotePath')}</th>
+                <th />
+                <th>{_('remoteState')}</th>
+                <th className='text-xs-right'>{_('remoteAction')}</th>
+              </tr>
+              {map(remotes.file, (remote, key) => <LocalRemote remote={remote} key={key} />)}
+            </tbody>
+          }
+          {!isEmpty(remotes.nfs) &&
+            <tbody>
+              <tr>
+                <th className='text-info'>{_('remoteTypeNfs')}</th>
+                <th>{_('remoteName')}</th>
+                <th>{_('remoteDevice')}</th>
+                <th />
+                <th>{_('remoteState')}</th>
+                <th className='text-xs-right'>{_('remoteAction')}</th>
+              </tr>
+              {map(remotes.nfs, (remote, key) => <NfsRemote remote={remote} key={key} />)}
+            </tbody>
+          }
+          {!isEmpty(remotes.smb) &&
+            <tbody>
+              <tr>
+                <th className='text-info'>{_('remoteTypeSmb')}</th>
+                <th>{_('remoteName')}</th>
+                <th>{_('remoteShare')}</th>
+                <th>{_('remoteAuth')}</th>
+                <th>{_('remoteState')}</th>
+                <th className='text-xs-right'>{_('remoteAction')}</th>
+              </tr>
+              {map(remotes.smb, (remote, key) => <SmbRemote remote={remote} key={key} />)}
+            </tbody>
+          }
+        </table>
+        <h2>{_('newRemote')}</h2>
+        <form id='newRemoteForm'>
+          <div className='form-group'>
+            <label htmlFor='newRemoteType'>{_('remoteType')}</label>
+            <select
+              id='newRemoteType'
+              className='form-control'
+              defaultValue={type}
+              onChange={event => { this._handleRemoteTypeSelection(event.target.value) }}
+              required
+            >
+              {map(remoteTypes, (label, key) => _({key}, label, message => <option value={key}>{message}</option>))}
+            </select>
+            {type === 'smb' && <em className='text-warning'>{_('remoteSmbWarningMessage')}</em>}
+          </div>
+          <div className='form-group'>
+            <input type='text' ref='name' className='form-control' placeholder={this.props.intl.formatMessage(messages.remoteMyNamePlaceHolder)} required />
+          </div>
+          {type === 'file' &&
+            <fieldset className='form-group'>
+              <div className='input-group'>
+                <span className='input-group-addon'>/</span>
+                <input type='text' ref='path' pattern='^(([^/]+)+(/[^/]+)*)?$' className='form-control' placeholder={this.props.intl.formatMessage(messages.remoteLocalPlaceHolderPath)} />
+              </div>
+            </fieldset>
+          }
+          {type === 'nfs' &&
+            <fieldset className='form-group'>
+              <div className='form-group'>
+                <input type='text' ref='host' className='form-control' placeholder={this.props.intl.formatMessage(messages.remoteNfsPlaceHolderHost)} required />
+              </div>
+              <div className='input-group'>
+                <span className='input-group-addon'>/</span>
+                <input type='text' ref='path' pattern='^(([^/]+)+(/[^/]+)*)?$' className='form-control' placeholder={this.props.intl.formatMessage(messages.remoteNfsPlaceHolderPath)} />
+              </div>
+            </fieldset>
+          }
+          {type === 'smb' &&
+            <fieldset className='form-group'>
+              <div className='input-group form-group'>
+                <span className='input-group-addon'>\\</span>
+                <input type='text' ref='host' pattern='^([^\\/]+)\\([^\\/]+)$' className='form-control' placeholder={this.props.intl.formatMessage(messages.remoteSmbPlaceHolderAddressShare)} required />
+                <span className='input-group-addon'>\</span>
+                <input type='text' ref='path' pattern='^(([^\\/]+)+(\\[^\\/]+)*)?$' className='form-control' placeholder={this.props.intl.formatMessage(messages.remoteSmbPlaceHolderRemotePath)} />
+              </div>
+              <div className='form-group'>
+                <input type='text' ref='username' className='form-control' placeholder={this.props.intl.formatMessage(messages.remoteSmbPlaceHolderUsername)} />
+              </div>
+              <div className='form-group'>
+                <input type='text' ref='password' className='form-control' placeholder={this.props.intl.formatMessage(messages.remoteSmbPlaceHolderPassword)} />
+              </div>
+              <div className='form-group'>
+                <input type='text' ref='domain' className='form-control' placeholder={this.props.intl.formatMessage(messages.remoteSmbPlaceHolderDomain)} required />
+              </div>
+            </fieldset>
+          }
+          <div className='form-group'>
+            <ActionButton type='submit' form='newRemoteForm' icon='save' btnStyle='primary' handler={this._checkNameExists}>
+              {_('savePluginConfiguration')}
+            </ActionButton>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
